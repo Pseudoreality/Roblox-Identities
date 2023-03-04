5a9670c7d30e871230f4e0e4e8d72d93e1cc1c03
Pseudoreality <92556981+Pseudoreality@users.noreply.github.com>
2023-03-04 14:38:08 -0600

    Create CheckPermissions.lua

---
+++ CheckPermissions.lua
@@ -0,0 +1,57 @@
+-- didn't use any luau for backwards compatibility
+-- basically pcall, but only returns it's success
+local function try(fn, ...)
+	return (pcall(fn, ...))
+end
+
+local securityChecks = {
+	{
+		name = "None",
+		number = 0,
+		canAccess = try(function() return game.Name end)
+	},
+	{
+		name = "PluginSecurity",
+		number = 1,
+		canAccess = try(function() return game:GetService("CoreGui").Name end)
+	},
+	-- RobloxPlace doesn't exist, skip
+	-- this is also why I have to provide the number, lel
+	{
+		name = "LocalUserSecurity",
+		number = 3,
+		canAccess = try(function() return game.DataCost end)
+	},
+	{
+		name = "WritePlayerSecurity",
+		number = 4,
+		canAccess = try(Instance.new, "Player")
+	},
+	{
+		name = "RobloxScriptSecurity",
+		number = 5,
+		canAccess = try(function() return game:GetService("CorePackages").Name end)
+	},
+	{
+		name = "RobloxSecurity",
+		number = 6,
+		canAccess = try(function() return Instance.new("SurfaceAppearance").TexturePack end)
+	},
+	{
+		name = "NotAccessibleSecurity",
+		number = 7,
+		canAccess = try(function() Instance.new("MeshPart").MeshId = "" end)
+	}
+}
+
+-- format everything
+local message = ""
+
+for index, securityInfo in pairs(securityChecks) do
+	local canAccess = securityInfo.canAccess and "Yes" or "No"
+	
+	message = message .. (securityInfo.name .. " (" .. securityInfo.number .. ") = " .. canAccess .. (index ~= #securityChecks and "\n" or ""))
+end
+
+printidentity()
+print(message)
